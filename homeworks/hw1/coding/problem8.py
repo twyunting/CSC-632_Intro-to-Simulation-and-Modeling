2bd3b0b1f1da79bafce98f063ae5399edd3cd74c
twyunting <yc6705a@american.edu>
2021-01-31 20:50:36 -0500

    tmp

--- homeworks/hw1/coding/problem8.py
+++ homeworks/hw1/coding/problem8.py
@@ -1,11 +1,43 @@
-# Python3 code to demonstrate 
-# the working of uniform() 
-  
-# for using uniform() 
+# importing library
+import numpy as np
+import matplotlib.pyplot as plt
 import random 
-  
-# initializing bounds  
-a = 4
-b = 9
 
-print(random.uniform(a, b)) 
+"""
+a=1
+b=100
+N=100  
+X1=np.random.uniform(a,b,N)
+plt.plot(X1)
+plt.show()
+plt.figure()
+plt.hist(X1, density=True, histtype='stepfilled', alpha=0.2)
+#plt.show()
+"""
+
+
+a = 0
+b = 51 # need to include 50 so we add up 1
+N = 500
+zeroToFifty = np.random.uniform(a, b, 500)
+plt.plot(zeroToFifty)
+plt.show()
+
+print(zeroToFifty)
+
+
+"""
+a=1
+b=100
+N=10000  
+X2=np.random.uniform(a,b,N)
+
+plt.figure()
+plt.plot(X2)
+plt.show()
+
+plt.figure()
+plt.hist(X2, density=True, histtype='stepfilled', alpha=0.2)
+plt.show()
+
+"""
