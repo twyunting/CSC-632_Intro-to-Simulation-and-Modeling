b68f9c729df472f9e21e4b21f487c20f1cd6cb20
twyunting <yc6705a@american.edu>
2021-01-31 22:13:32 -0500

    Update problem8.py

--- homeworks/hw1/coding/problem8.py
+++ homeworks/hw1/coding/problem8.py
@@ -3,49 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random 
 
-
 a = 0
-b = 50 # needs to include 50 so we add up 1
+b = 51 # needs to include 50 so we add up 1
 n = 50
 
+# create a function
 def zeroToFifty(a, b, n):
 	zeroToFifty = np.random.uniform(a, b, n)
-	plt.hist(zeroToFifty, density = True, histtype = 'stepfilled', alpha = 0.2)
+	# stepfilled' generates a lineplot that is by default filled
+	# density = True: draw and return a probability density:
+	plt.hist(zeroToFifty, density = True, histtype = 'stepfilled', alpha = 0.5)
+	# Display all open figures
 	plt.show()
 
+# Using for loop to repeat the same graph by replicating 500 samples 
 for i in range(5):
 	zeroToFifty(a, b, n)
-
-
-
-# print(zeroToFifty)
-
-"""
-a=1
-b=100
-N=100  
-X1=np.random.uniform(a,b,N)
-plt.plot(X1)
-plt.show()
-plt.figure()
-plt.hist(X1, density=True, histtype='stepfilled', alpha=0.2)
-#plt.show()
-"""
-
-
-
-"""
-a=1
-b=100
-N=10000  
-X2=np.random.uniform(a,b,N)
-
-plt.figure()
-plt.plot(X2)
-plt.show()
-
-plt.figure()
-plt.hist(X2, density=True, histtype='stepfilled', alpha=0.2)
-plt.show()
-
-"""
