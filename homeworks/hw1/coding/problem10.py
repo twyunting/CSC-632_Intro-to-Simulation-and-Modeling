e4b002f658f8d87fbb6c338892fb0eea38443ff7
twyunting <yc6705a@american.edu>
2021-02-04 13:41:39 -0500

    Update problem10.py

--- homeworks/hw1/coding/problem10.py
+++ homeworks/hw1/coding/problem10.py
@@ -2,10 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-mu = 25
-sigma = 3
-Y = np.random.normal(mu, sigma)
-PlotY = sns.distplot(Y)
-plt.figure()
-plt.show()
-print(Y)
+def probabilityDistribution(mu, sigma):
+	Y = np.random.normal(mu, sigma)
+	PlotY = sns.distplot(Y)
+	plt.figure()
+	plt.show()
+	plt.hist(zeroToFifty, density = True, histtype = 'stepfilled', alpha = 0.5)
+	# Display all open figures
+	plt.show()
+
+
+
+probabilityDistribution(25, 3)
+probabilityDistribution(25, 4)
+probabilityDistribution(25, 5)
