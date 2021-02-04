5990ea846aa12e63a52f3b0f98e4e7e2f05d8200
twyunting <yc6705a@american.edu>
2021-02-04 15:58:44 -0500

    Update problem9.py

--- homeworks/hw1/coding/problem9.py
+++ homeworks/hw1/coding/problem9.py
@@ -7,9 +7,8 @@ p = 0.1 # p is the probability of success for each experiment
 
 P1 = np.random.binomial(n, p, N)
 plt.plot(P1)
-
 plt.figure()
-plt.hist(P1, density=True, alpha=0.8, histtype='bar', color = 'green', ec='black')
+plt.hist(P1, density = True, alpha = 0.8, histtype = 'bar', color = 'red', ec = 'black')
 plt.show()
 
 # Find probability of having 5 successes (that is p (X = 5))
@@ -18,7 +17,6 @@ res = []
 for i, j in enumerate(P1):
 	if j == 5:
 	 res.append(i+1) # add the Xth = 5 in the new array
-
 
 print(res)
 
